84f3698aafc4224be959ef3727c62a4435e2af2d
John Mitchell <jrmmba@outlook.com>
2020-07-20 08:12:19 -0700

    Updating Documentation

--- sampleemps_swagger/src/main/java/com/lambdaschool/sampleemps/controllers/EmployeeController.java
+++ sampleemps_swagger/src/main/java/com/lambdaschool/sampleemps/controllers/EmployeeController.java
@@ -1,18 +1,20 @@
 package com.lambdaschool.sampleemps.controllers;
 
 import com.lambdaschool.sampleemps.models.Employee;
-import com.lambdaschool.sampleemps.models.ErrorDetail;
 import com.lambdaschool.sampleemps.services.EmployeeService;
 import com.lambdaschool.sampleemps.views.EmpNameCountJobs;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -26,127 +28,32 @@ public class EmployeeController
     @Autowired
     private EmployeeService employeeService;
 
-    @ApiOperation(value = "returns all Employees",
-            response = Employee.class,
-            responseContainer = "List")
     @GetMapping(value = "/employees")
     public ResponseEntity<?> listAllEmployees()
     {
         List<Employee> myEmployees = employeeService.findAllEmployees();
         return new ResponseEntity<>(myEmployees,
-            HttpStatus.OK);
-    }
-
-    @ApiOperation(value = "Retrieve an employee based off of employee id",
-            response = Employee.class)
-    @ApiResponses(value = {@ApiResponse(code = 200,
-            message = "Employee Found",
-            response = Employee.class), @ApiResponse(code = 404,
-            message = "Employee Not Found",
-            response = ErrorDetail.class)})
-    @GetMapping(value = "/employee/{employeeid}")
-    public ResponseEntity<?> getEmployeeById(
-            @ApiParam(value = "Employee id",
-                    required = true,
-                    example = "4")
-            @PathVariable
-                    long employeeid)
-    {
-        Employee e = employeeService.findEmployeeById(employeeid);
-        return new ResponseEntity<>(e, HttpStatus.OK);
+                                    HttpStatus.OK);
     }
 
     @GetMapping(value = "/employeename/{subname}")
     public ResponseEntity<?> listEmployeesWithName(
-        @PathVariable
-            String subname)
+            @PathVariable
+                    String subname)
     {
         List<Employee> myEmployees = employeeService.findEmployeeNameContaining(subname);
         return new ResponseEntity<>(myEmployees,
-            HttpStatus.OK);
+                                    HttpStatus.OK);
     }
 
     @GetMapping(value = "/employeeemail/{subemail}")
     public ResponseEntity<?> listEmployeesWithEmail(
-        @PathVariable
-            String subemail)
+            @PathVariable
+                    String subemail)
     {
         List<Employee> myEmployees = employeeService.findEmployeeEmailContaining(subemail);
         return new ResponseEntity<>(myEmployees,
-            HttpStatus.OK);
-    }
-
-    @PostMapping(value = "/employee",
-        consumes = {"application/json"})
-    public ResponseEntity<?> addNewEmployee(
-        @Valid
-        @RequestBody
-            Employee newEmployee)
-    {
-        // ids are not recognized by the Post method
-        newEmployee.setEmployeeid(0);
-        newEmployee = employeeService.save(newEmployee);
-
-        // set the location header for the newly created resource
-        HttpHeaders responseHeaders = new HttpHeaders();
-        URI newEmployeeURI = ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{employeeid}")
-            .buildAndExpand(newEmployee.getEmployeeid())
-            .toUri();
-        responseHeaders.setLocation(newEmployeeURI);
-
-        return new ResponseEntity<>(null,
-            responseHeaders,
-            HttpStatus.CREATED);
-    }
-
-    @ApiOperation(value = "updates an employee given in the request body",
-            response = Void.class)
-    @ApiResponses(value = {@ApiResponse(code = 200,
-            message = "Employee Found",
-            response = Void.class), @ApiResponse(code = 404,
-            message = "Employeer Not Found",
-            response = ErrorDetail.class)})
-    @PutMapping(value = "/employee/{employeeid}",
-        consumes = {"application/json"})
-    public ResponseEntity<?> updateFullEmployee(
-        @ApiParam(value = "a full employee object",
-                required = true)
-        @Valid
-        @RequestBody
-            Employee updateEmployee,
-        @ApiParam(value = "employeeid",
-                required = true,
-                example = "4")
-        @PathVariable
-            long employeeid)
-    {
-        updateEmployee.setEmployeeid(employeeid);
-        employeeService.save(updateEmployee);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @PatchMapping(value = "/employee/{employeeid}",
-        consumes = {"application/json"})
-    public ResponseEntity<?> updateEmployee(
-        @RequestBody
-            Employee updateEmployee,
-        @PathVariable
-            long employeeid)
-    {
-        employeeService.update(updateEmployee,
-            employeeid);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @DeleteMapping(value = "/employee/{employeeid}")
-    public ResponseEntity<?> deleteEmployeeById(
-        @PathVariable
-            long employeeid)
-    {
-        employeeService.delete(employeeid);
-        return new ResponseEntity<>(HttpStatus.OK);
+                                    HttpStatus.OK);
     }
 
     @GetMapping(value = "/job/counts")
@@ -154,34 +61,58 @@ public class EmployeeController
     {
         List<EmpNameCountJobs> myEmployees = employeeService.getEmpNameCountJobs();
         return new ResponseEntity<>(myEmployees,
-            HttpStatus.OK);
+                                    HttpStatus.OK);
     }
 
-    @DeleteMapping(value = "/employee/{employeeid}/jobtitle/{jobtitleid}")
-    public ResponseEntity<?> deleteEmployeeJobTitlesByid(
-        @PathVariable
-            long employeeid,
-        @PathVariable
-            long jobtitleid)
+    @PostMapping(value = "/employee",
+            consumes = {"application/json"})
+    public ResponseEntity<?> addNewEmployee(
+            @Valid
+            @RequestBody
+                    Employee newEmployee)
     {
-        employeeService.deleteEmpJobTitle(employeeid,
-        jobtitleid);
+        // ids are not recognized by the Post method
+        newEmployee.setEmployeeid(0);
+        newEmployee = employeeService.save(newEmployee);
+
+        // set the location header for the newly created resource
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newEmployeeURI = ServletUriComponentsBuilder.fromCurrentRequest() // get the URI for this request
+                .path("/{employeeid}") // add to it a path variable
+                .buildAndExpand(newEmployee.getEmployeeid()) // populate that path variable with the newly created restaurant id
+                .toUri(); // convert that work into a human readable URI
+        responseHeaders.setLocation(newEmployeeURI); // in the header, set the location location to that URI
+        return new ResponseEntity<>(null,
+                                    responseHeaders,
+                                    HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/employee/{employeeid}",
+            consumes = {"application/json"})
+    public ResponseEntity<?> updateFullEmployee(
+            @Valid // verifies the give employee record is valid
+            @RequestBody
+                    Employee updateEmployee, // the Jackson dependency converts from JSON to Java Objects
+            @PathVariable
+                    long employeeid) // the primary key of the employee record to replace
+    {
+        updateEmployee.setEmployeeid(employeeid); // set the primary key to the one from the path variable
+        employeeService.save(updateEmployee);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping(value = "/employee/{employeeid}/jobtitle/{jobtitleid}/manager/{manager}")
-    public ResponseEntity<?> addEmployeeJobTitlesByid(
-        @PathVariable
-            long employeeid,
-        @PathVariable
-            long jobtitleid,
-        @PathVariable
-            String manager)
+    @PatchMapping(value = "/employee/{employeeid}",
+            consumes = {"application/json"})
+    public ResponseEntity<?> updateEmployee(
+            @RequestBody
+                    Employee updateEmployee,
+            @PathVariable
+                    long employeeid)
     {
-        employeeService.addEmpJobTitle(employeeid,
-            jobtitleid, manager);
-
+        employeeService.update(updateEmployee,
+                               employeeid);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
--- sampleemps_swagger/src/main/java/com/lambdaschool/sampleemps/models/Employee.java
+++ sampleemps_swagger/src/main/java/com/lambdaschool/sampleemps/models/Employee.java
@@ -1,8 +1,6 @@
 package com.lambdaschool.sampleemps.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -10,42 +8,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-@ApiModel(value = "Employee",
-        description = "Yes an actual employee record")
 @Entity
 @Table(name = "employees")
 @JsonIgnoreProperties(value = {"hasvalueforsalary"})
-public class Employee extends Auditable
+public class Employee
 {
-    @ApiModelProperty(name = "employee id",
-            value = "primary key for employee",
-            required = true,
-            example = "1")
     @Id // The primary key
     @GeneratedValue(strategy = GenerationType.AUTO) // We will let the database decide how to generate it
     private long employeeid; // long so we can have many rows
 
-    @ApiModelProperty(name = "employee name",
-            value = "full name of employee",
-            required = true,
-            example = "Best Employee")
     @Column(nullable = false,
             unique = true)
     private String name;
 
     @Transient
     public boolean hasvalueforsalary = false;
-
-    @ApiModelProperty(name = "employee name",
-            value = "salary of employee",
-            required = false,
-            example = "100000")
     private double salary;
 
     /*
@@ -55,29 +43,26 @@ public class Employee extends Auditable
      * the EmployeeTitle join table but does not affect the JobTitle table.
      */
     @OneToMany(mappedBy = "emp",
-        cascade = CascadeType.ALL)
+            cascade = CascadeType.ALL)
     /*
      * When displaying EmployeeTitles from the Employee class, do not display the employee again.
      * However do allow for data to be added to the emp field in EmployeeTitles
      */
     @JsonIgnoreProperties(value = "emp",
-        allowSetters = true)
+            allowSetters = true)
     /*
      * We know all of this works with EmployeeTitles because that is the class of the field name that making the One To Many relationship!
      * This array contains the list of EmployeeTitles assigned to this Employee
      */
-    private List<EmployeeTitles> jobnames = new ArrayList<>();
+    private Set<EmployeeTitles> jobnames = new HashSet<>();
 
-    /*
-     * This starts the One To Many relation of employee to emails
-     */
     @OneToMany(mappedBy = "employee",
-        cascade = CascadeType.ALL,
-        // when adding, reading, updating, and delete, the operations should affect the emails table as well)
-        orphanRemoval = true)
+            cascade = CascadeType.ALL,
+            // when adding, reading, updating, and delete, the operations should affect the emails table as well)
+            orphanRemoval = true)
     // if we find a email that has a reference to an employee that does not exist, delete that email record
-    @JsonIgnoreProperties(value = "employee",
-        allowSetters = true)
+    // we want to ignore, not display, the employee object found in Email
+    @JsonIgnoreProperties(value = "employee")
     private List<Email> emails = new ArrayList<>();
 
     public Employee()
@@ -86,21 +71,11 @@ public class Employee extends Auditable
     }
 
     public Employee(
-        String name,
-        double salary,
-        List<EmployeeTitles> jobnames)
+            String name,
+            double salary)
     {
         this.name = name;
         this.salary = salary;
-
-        /*
-         * Force the list of roles to be associated with this new employee object!
-         */
-        for (EmployeeTitles et : jobnames)
-        {
-            et.setEmp(this);
-        }
-        this.jobnames = jobnames;
     }
 
     public long getEmployeeid()
@@ -144,31 +119,13 @@ public class Employee extends Auditable
         this.emails = emails;
     }
 
-    /*
-     * We need a getter for the new One To Many relations
-     */
-    public List<EmployeeTitles> getJobnames()
+    public Set<EmployeeTitles> getJobnames()
     {
         return jobnames;
     }
 
-    /*
-     * We need a setter for the new One To Many relations
-     */
-    public void setJobnames(List<EmployeeTitles> jobnames)
+    public void setJobnames(Set<EmployeeTitles> jobnames)
     {
         this.jobnames = jobnames;
-    }
-
-    /*
-     * Due to the new One To Many relation, we need a new way to add a Job Title to the employee
-     */
-    public void addJobTitle(
-        JobTitle jobTitle,
-        String manager)
-    {
-        jobnames.add(new EmployeeTitles(this,
-            jobTitle,
-            manager));
     }
 }
